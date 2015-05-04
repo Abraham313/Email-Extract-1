cbe316f13042f82c8ff3db5835f4646ac378c3ca
Amol <amoludage@joshsoftware.com>
2015-05-04 12:08:26 +0530

    changes in push into csv file and name of functions

--- sent_mails.rb
+++ sent_mails.rb
@@ -1,42 +1,48 @@
 #https://github.com/gmailgem/gmail --gem used
+require 'csv'
+require 'io/console' #for hidden password input
 require 'gmail'
 
 class Email
   def initialize(user_name, password)
     @gmail = Gmail.connect(user_name, password)
-    @array = Hash.new
+    @array = []
   end
 
-  def get_mails
-    File.open('email.csv', 'w') do |file|
-      file << ["Email, Name"]
-
-      @gmail.label("Sent").emails.each do |email|
-        recipients_from_to(email[:to]) if email.message.to
-        recipients_from_to(email[:cc]) if email.message.cc
-      end
-
-      file << @array.to_a
-
-      @gmail.logout
-      file.close
+  def get_recipients
+    @gmail.label("Sent").emails.each do |email|
+      push_recipients_to_array(email[:to]) if email.message.to
+      push_recipients_to_array(email[:cc]) if email.message.cc
     end
+
+    add_recipients_to_file
+    @gmail.logout
   end
 
   private
 
-  def recipients_from_to(email)
+  def push_recipients_to_array(email)
     email.each do |mail|
-      @array[mail.mailbox.concat("@").concat(mail.host)] = mail.first
+      @array << { "Email" =>  mail.mailbox.concat("@").concat(mail.host), "Name" => mail.first }
+    end
+  end
+
+  def add_recipients_to_file
+    header =  ["Email", "Name"]
+    File.open('email.csv', 'w') do |file|
+      file.puts header.to_csv
+      @array.uniq.each do |arr|
+        file.puts header.map {|h| arr[h]}.to_csv
+      end
+      file.close
     end
   end
 
 end
-
 puts "Enter user name:"
 user_name = gets.chomp
 puts "Enter password:"
-password = gets.chomp
+password = STDIN.noecho(&:gets).chomp
 
 @email = Email.new(user_name, password)
-@email.get_mails
+@email.get_recipients
