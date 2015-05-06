b3c08859f75a1f6982445d135fb67680751c74c3
Amol <amoludage@joshsoftware.com>
2015-05-06 11:26:35 +0530

    sample specs for login

--- spec/sent_mails_spec.rb
+++ spec/sent_mails_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Email do
     expect(@gmail.logged_in?).to eq true
   end
 
+  it 'response for invalid data' do
+    gmail = Gmail.connect("amol@nd.cl", "djdweiuuo")
+    expect(gmail.logged_in?).to eq false
+  end
+
+  it '#get_recipients' do
+    @email = Email.new("amoludage7@gmail.com", "9860666535143")
+    expect(@email.get_recipients.name).to eq "OK"
+  end
 end
