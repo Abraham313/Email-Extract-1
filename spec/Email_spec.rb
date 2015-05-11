f3ef93199671fb37140f54ee2c80f967b726a026
Amol <amoludage@joshsoftware.com>
2015-05-11 19:43:24 +0530

    write some specs

--- spec/Email_spec.rb
+++ spec/Email_spec.rb
@@ -4,22 +4,27 @@ require_relative '../lib/Email'
 RSpec.describe Email do
 
   it "properly logs in to valid Gmail account" do
-    user = Gmail.connect("amoludage7", "9860666535143")
-    expect(user).to be_logged_in
+    email = Email.new("amoludage7", "9860666535143")
+    expect(email.instance_variable_get('@gmail')).to be_logged_in
+    expect(email.instance_variable_get('@recipients')).to be_empty
   end
 
-  it "raises error when given Gmail account is invalid" do
+  it "Not logs in to invalid Gmail account credentials" do
+    email = Email.new("amoludage7", "9860666535143")
+    expect(email.instance_variable_get('@gmail')).to be_logged_in
+  end
+
+  it "raises error when given Gmail account credentials is invalid" do
     expect { Email.new("amoludage70", "986066535143") }.to raise_error(Gmail::Client::AuthorizationError)
   end
 
   context "#get_recipients" do
     before do
-      @gmail = Gmail.connect("amoludage7", "9860666535143")
-      @recipients = []
+      @email = Email.new("amoludage7", "9860666535143")
+      @email.get_recipients
     end
 
-    it { expect(@recipients.empty?).to eq true }
-    it { expect(@gmail.logged_in?).to eq true }
-    it { expect(@gmail.username).to eq "amoludage7@gmail.com"}
+    it { expect(@email.instance_variable_get('@gmail')).not_to be_logged_in }
+    it { expect(@email.instance_variable_get('@recipients')).not_to be_empty }
   end
 end
