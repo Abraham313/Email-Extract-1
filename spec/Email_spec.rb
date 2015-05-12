1312bd68eff5500be4da5cce38de506649d4c5b0
Amol <amoludage@joshsoftware.com>
2015-05-12 19:19:56 +0530

    write specs for gmail login and open file

--- spec/Email_spec.rb
+++ spec/Email_spec.rb
@@ -2,29 +2,35 @@ require "spec_helper"
 require_relative '../lib/Email'
 
 RSpec.describe Email do
-
-  it "properly logs in to valid Gmail account" do
-    email = Email.new("amoludage7", "9860666535143")
-    expect(email.instance_variable_get('@gmail')).to be_logged_in
-    expect(email.instance_variable_get('@recipients')).to be_empty
+  before(:each) do
+    @email = Email.new("amoludage7", "9860666535143")
   end
 
-  it "Not logs in to invalid Gmail account credentials" do
-    email = Email.new("amoludage7", "9860666535143")
-    expect(email.instance_variable_get('@gmail')).to be_logged_in
+  it "properly logs in to valid Gmail account" do
+    expect(@email.instance_variable_get('@gmail')).to be_logged_in
   end
 
   it "raises error when given Gmail account credentials is invalid" do
     expect { Email.new("amoludage70", "986066535143") }.to raise_error(Gmail::Client::AuthorizationError)
   end
 
-  context "#get_recipients" do
-    before do
-      @email = Email.new("amoludage7", "9860666535143")
-      @email.get_recipients
-    end
+  it "push email id and name into array after #get_recipients called" do
+    recipients = @email.instance_variable_get('@recipients')
+    gmail = @email.instance_variable_get('@gmail')
+    expect(recipients).to be_empty
+    @email.get_recipients
+    expect(recipients).not_to be_empty
+    expect(recipients.uniq { |arr| arr['Email'] }.length).to eq recipients.length
+  end
 
-    it { expect(@email.instance_variable_get('@gmail')).not_to be_logged_in }
-    it { expect(@email.instance_variable_get('@recipients')).not_to be_empty }
+  it "log out after #get_recipients complete" do
+    gmail = @email.instance_variable_get('@gmail')
+    @email.get_recipients
+    expect(gmail).not_to be_logged_in
+  end
+
+  it "should open file and write into the file after #get_recipients called" do
+    @email.get_recipients
+    allow(File).to receive(:open).with('eail.csv', 'wb')
   end
 end
