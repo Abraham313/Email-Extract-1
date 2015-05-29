dd6b5dc1c7e38c3e6c19e8e6f2edec875b2120cf
Amol <amoludage@joshsoftware.com>
2015-05-29 15:04:18 +0530

    use interrupt handler for INT signal

--- lib/email.rb
+++ lib/email.rb
@@ -1,20 +1,36 @@
 #https://github.com/gmailgem/gmail --gem used
 require 'gmail'
 require 'csv'
+require 'byebug'
 
 class Email
   def initialize(user_name, password)
     @gmail = Gmail.connect!(user_name, password)
-    @recipients = []
+    @recipients = CSV.read('recipients.csv').to_h
   end
 
   def get_recipients
-    @gmail.label("Sent").emails.each do |email|
+    previous_email = nil
+
+    trap("INT") do
+      File.open('interrupt.txt', 'wb') { |file| file.write(Marshal.dump(previous_email.date)) } if previous_email
+      add_recipients_to_file if @recipients
+      exit
+    end
+
+    begin
+      date = Marshal.load(File.read('interrupt.txt'))
+    rescue ArgumentError
+      p 'Initial fetching...'
+    end
+
+    @gmail.label("Sent").emails_in_batches(after: date) do |email|
+      previous_email = email
       push_recipients_to_array(email.to) if email.message.to
       push_recipients_to_array(email.cc) if email.message.cc
     end
 
-    check_recipients_present
+    add_recipients_to_file if @recipients
     @gmail.logout
   end
 
@@ -22,22 +38,17 @@ class Email
 
   def push_recipients_to_array(email)
     email.each do |mail|
-      @recipients << { "Email" =>  mail.mailbox.concat("@").concat(mail.host), "Name" => mail.first }
-    end
-  end
-
-  def check_recipients_present
-    unless @recipients.empty?
-      @recipients.uniq! {|arr| arr["Email"]}
-      add_recipients_to_file
+      @recipients[mail.mailbox.concat('@').concat(mail.host)] =  mail.first
+      puts 'writting...'
+      sleep 0.8
     end
   end
 
   def add_recipients_to_file
     headers = ['Email', 'Name']
-    CSV.open('recipients.csv', 'w', {write_headers: true ,headers: headers}) do |csv|
+    CSV.open('recipients.csv', 'w', {write_headers: true, headers: headers}) do |csv|
       @recipients.each do |arr|
-        csv << arr.values_at(*headers)
+        csv << arr
       end
     end
   end
